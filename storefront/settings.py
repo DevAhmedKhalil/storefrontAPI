655ea10cd48d36e8652c5aaf1ed985928ca7a0f1
DevAhmedKhalil <dev.akhalil0101@gmail.com>
2024-04-28 15:11:42 +0300

    Finish Setting Up DataBase

--- storefront/settings.py
+++ storefront/settings.py
@@ -86,8 +86,10 @@ WSGI_APPLICATION = "storefront.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "storefront",
+        "USER": "root",
+        "PASSWORD": "root",
     }
 }
 
