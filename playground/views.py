4b0ab31bacce4a54d379d08f5dfd446d6dfa03ba
DevAhmedKhalil <dev.akhalil0101@gmail.com>
2024-05-01 14:44:07 +0300

    Make Custom Managers.

--- playground/views.py
+++ playground/views.py
@@ -5,10 +5,6 @@ from tags.models import TaggedItem
 
 
 def say_hello(request):
-    content_type = ContentType.objects.get_for_model(Product)  # 11,store,product
-
-    queryset = TaggedItem.objects.select_related("tag").filter(
-        content_type=content_type, object_id=1
-    )
+    queryset = TaggedItem.objects.get_tags_for(Product, 1)
 
     return render(request, "hello.html", {"name": "Ahmed", "tags": list(queryset)})
