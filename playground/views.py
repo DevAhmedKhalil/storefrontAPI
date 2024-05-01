fd0b1382a2c1e20c7d9d8b28322dc4df198ef58b
DevAhmedKhalil <dev.akhalil0101@gmail.com>
2024-05-01 18:40:08 +0300

    Updating Objects.

--- playground/views.py
+++ playground/views.py
@@ -3,21 +3,11 @@ from store.models import Product, Collection
 
 
 def say_hello(request):
-    # 1) Best way to creatin object
-    collection = Collection()
-    collection.title = "Video Games"
-    collection.featured_product = Product(pk=1)
-    # OR
-    # collection.featured_product_id = 1
-    collection.save()
-
-    # # 2) Using constractor
-    # collection = Collection(title="Test Collection", featured_product_id=1)
+    # # 1) Updating object -> use .get to update only 'title'
+    # collection = Collection.objects.get(pk=11)
+    # collection.title = "Games"
     # collection.save()
 
-    # 3) Using create method
-    # collection = Collection.objects.create(
-    #     title="Test Collection", featured_product_id=1
-    # )
-
+    # 2) Update object using update method
+    Collection.objects.filter(pk=11).update(featured_product=None)
     return render(request, "hello.html", {"name": "Ahmed"})
