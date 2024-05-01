eddef4b07315396f0d0603971496f61bee3a86ed
DevAhmedKhalil <dev.akhalil0101@gmail.com>
2024-05-01 19:15:48 +0300

    Executing Raw SQL Queries.

--- playground/views.py
+++ playground/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
-from django.db import transaction
+from django.db import transaction, connection
 from store.models import Product, Collection, Order, OrderItem
 
 
 def say_hello(request):
-    with transaction.atomic():
-        # Parent Record
-        order = Order()
-        order.customer_id = 1
-        order.save()
+    # # Executing Raw SQL Queries
+    # # 1) Using objects.raw() method
+    # queryset = Product.objects.raw("SELECT id, title FROM store_product")
 
-        # Child Record
-        item = OrderItem()
-        item.order = order
-        item.product_id = 1
-        item.quantity = 1
-        item.unit_price = 10
-        item.save()
+    # # 2) Using connection module
+    # cursor = connection.cursor
+    # cursor.execute("SELECT id, title FROM store_product")
+    # cursor.close()  # use try finlly block
+
+    # 3) Using connection and 'with as' statement
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT id, title FROM store_product")
+        # cursor.callproc("get_customers", [1, 2])
 
     return render(request, "hello.html", {"name": "Ahmed"})
