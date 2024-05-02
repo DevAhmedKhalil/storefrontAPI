45193c4b931adcbc4c849cf7226004799523fdf1
DevAhmedKhalil <dev.akhalil0101@gmail.com>
2024-05-02 06:40:24 +0300

    Setting up the admin site, Registering Models.

--- store/models.py
+++ store/models.py
@@ -12,6 +12,12 @@ class Collection(models.Model):
         "Product", on_delete=models.SET_NULL, null=True, related_name="+"
     )
 
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ["title"]
+
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -22,6 +28,12 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ["title"]
 
 
 class Customer(models.Model):
