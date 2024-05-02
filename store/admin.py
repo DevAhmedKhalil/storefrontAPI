4d6ba24932e42235f7c4fc0e3cf2e8072ff7409e
DevAhmedKhalil <dev.akhalil0101@gmail.com>
2024-05-02 09:07:19 +0300

    Selecting related objects, Adding filtering to list page.

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,34 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
 from . import models
+
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = "inventory"
+    parameter_name = "inventory"
+
+    def lookups(self, request, model_admin):
+        return [("<10", "Low"), (">=10", "Ok")]
+
+    def queryset(self, request: Any, queryset: QuerySet[Any]) -> QuerySet[Any] | None:
+        if self.value() == "<10":
+            return queryset.filter(inventory__lt=10)
+        elif self.value() == ">=10":
+            return queryset.filter(inventory__gte=10)
+        return None
 
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ["title", "unit_price", "inventory_status"]
+    list_display = ["title", "unit_price", "inventory_status", "collection_title"]
     list_editable = ["unit_price"]
+    list_filter = ["collection", "last_update", InventoryFilter]
     list_per_page = 10
+    list_select_related = ["collection"]
+
+    def collection_title(self, product):
+        return product.collection.title
 
     @admin.display(ordering="inventory")
     def inventory_status(self, product):
@@ -23,7 +45,10 @@ class CustomerAdmin(admin.ModelAdmin):
     list_per_page = 10
 
 
-# Register your models here.
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "placed_at", "customer"]
+
 
 admin.site.register(models.Collection)
 # admin.site.register(models.Product, ProductAdmin)
