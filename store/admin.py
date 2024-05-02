f84d1055e796b343ceda7c21f5a556556485ae6b
DevAhmedKhalil <dev.akhalil0101@gmail.com>
2024-05-02 07:36:15 +0300

    Customizing List Page, Adding Computed Columns

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
 from . import models
 
-# Register your models here.
-admin.site.register(models.Collection)
 
-admin.site.register(models.Product)
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "unit_price", "inventory_status"]
+    list_editable = ["unit_price"]
+    list_per_page = 10
+
+    @admin.display(ordering="inventory")
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return "Low"
+        return "Ok"
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "membership"]
+    list_editable = ["membership"]
+    ordering = ["first_name", "last_name"]
+    list_per_page = 10
+
+
+# Register your models here.
+
+admin.site.register(models.Collection)
+# admin.site.register(models.Product, ProductAdmin)
