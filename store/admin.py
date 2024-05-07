8655e06f8a91f3171e5ba85685554809743f41db
DevAhmedKhalil <akhalil01@gmail.com>
2024-05-07 18:10:49 +0300

    Use Generic Relations In tags app

--- store/admin.py
+++ store/admin.py
@@ -1,75 +1,122 @@
 from django.contrib import admin, messages
+from django.contrib.contenttypes.admin import GenericTabularInline
+from django.db.models.aggregates import Count
 from django.db.models.query import QuerySet
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
+from tags.models import TaggedItem
 from . import models
 
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low')
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+
+
+class TagInline(GenericTabularInline):
+    autocomplete_fields = ['tag']
+    model= TaggedItem
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    autocomplete_fields = ["collection"]
+    autocomplete_fields = ['collection']
     prepopulated_fields = {
-        "slug": ["title"],
+        'slug': ['title']
     }
-    search_fields = ["title"]  # Add this line to specify search fields
-    # fields = ["title", "slug"]
-    # exclude = ["promotions"]  # Opposite of fields
-    # readonly_fields = ["title"]
-    actions = ["clear_inventory"]
-    list_display = ["title", "unit_price", "inventory_status", "collection_title"]
-    list_editable = ["unit_price"]
-    list_filter = ["collection", "last_update", "inventory"]
+    actions = ['clear_inventory']
+    inlines = [TagInline]
+    list_display = ['title', 'unit_price',
+                    'inventory_status', 'collection_title']
+    list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 10
-    list_select_related = ["collection"]
+    list_select_related = ['collection']
+    search_fields = ['title']
 
     def collection_title(self, product):
         return product.collection.title
 
-    @admin.display(ordering="inventory")
+    @admin.display(ordering='inventory')
     def inventory_status(self, product):
-        return "Low" if product.inventory < 10 else "Ok"
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
 
-    @admin.action(description="Clear inventory")
+    @admin.action(description='Clear inventory')
     def clear_inventory(self, request, queryset):
-        # Update the inventory of selected products to 0
         updated_count = queryset.update(inventory=0)
         self.message_user(
             request,
-            f"{updated_count} products were successfully updated.",
-            messages.SUCCESS,
+            f'{updated_count} products were successfully updated.',
+            messages.ERROR
+        )
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['featured_product']
+    list_display = ['title', 'products_count']
+    search_fields = ['title']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = (
+            reverse('admin:store_product_changelist')
+            + '?'
+            + urlencode({
+                'collection__id': str(collection.id)
+            }))
+        return format_html('<a href="{}">{} Products</a>', url, collection.products_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
         )
 
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    search_fields = [""]
-    list_display = ["first_name", "last_name", "membership"]
-    list_editable = ["membership"]
-    ordering = ["first_name", "last_name"]
+    list_display = ['first_name', 'last_name',  'membership', 'orders']
+    list_editable = ['membership']
     list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+
+    @admin.display(ordering='orders_count')
+    def orders(self, customer):
+        url = (
+            reverse('admin:store_order_changelist')
+            + '?'
+            + urlencode({
+                'customer__id': str(customer.id)
+            }))
+        return format_html('<a href="{}">{} Orders</a>', url, customer.orders_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
 
 
-# admin.site.register(models.Collection)
-@admin.register(models.Collection)
-class CollectionAdmin(admin.ModelAdmin):
-    list_display = ["title", "products_count"]
-    search_fields = ["title"]
-
-    @admin.display(ordering="products_count")
-    def products_count(self, collection):
-        return collection.product_set.count()
-
-
-# ORDER ITEMS => Table to add orders of products
-class OrderItemInline(admin.StackedInline):
-    # class OrderItemInline(admin.TabularInline):
-    autocomplete_fields = ["product"]
-    model = models.OrderItem
-    extra = 0
+class OrderItemInline(admin.TabularInline):
+    autocomplete_fields = ['product']
     min_num = 1
     max_num = 10
+    model = models.OrderItem
+    extra = 0
 
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
-    autocomplete_fields = ["customer"]
+    autocomplete_fields = ['customer']
     inlines = [OrderItemInline]
-    list_display = ["id", "placed_at", "customer"]
+    list_display = ['id', 'placed_at', 'customer']
