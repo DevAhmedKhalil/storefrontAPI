f4d54bf06fa213b95a97afbdc85a82d612127208
DevAhmedKhalil <akhalil01@gmail.com>
2024-05-07 18:38:31 +0300

    Extending Pluggable Apps, Finish Part-1

---
+++ store_custom/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
+from store.admin import ProductAdmin
+from store.models import Product
+from tags.models import TaggedItem
+
+
+# We will compine features from two plugable apps
+class TagInline(GenericTabularInline):
+    autocomplete_fields = ["tag"]
+    model = TaggedItem
+
+
+class CustomProductAdmin(ProductAdmin):
+    inlines = [TagInline]
+
+
+admin.site.unregister(Product)
+admin.site.register(Product, CustomProductAdmin)
