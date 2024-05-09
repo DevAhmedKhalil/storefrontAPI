d22c4b418cc2dbe73f5d174e4178cad7ce7812b6
DevAhmedKhalil <akhalil01@gmail.com>
2024-05-09 06:12:34 +0300

    Models Serializers.

--- store/serializers.py
+++ store/serializers.py
@@ -3,27 +3,31 @@ from store.models import Product, Collection
 from rest_framework import serializers
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ["id", "title"]
+
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(
-        max_digits=6, decimal_places=2, source="unit_price"
-    )
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "title", "unit_price", "price_with_tax", "collection"]
+
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
-    # collection = serializers.PrimaryKeyRelatedField( # Way (1) it returns the ID of the collection
-    #     queryset=Collection.objects.all(),
+
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(
+    #     max_digits=6, decimal_places=2, source="unit_price"
     # )
-    # collection = serializers.StringRelatedField()    # Way (2)
-    # collection = CollectionSerializer()              # Way (3)
-    collection = serializers.HyperlinkedRelatedField(  # Way (4)
-        queryset=Collection.objects.all(),
-        view_name="collection-detail",
-    )
+    # collection = serializers.HyperlinkedRelatedField(  # Way (4)
+    #     queryset=Collection.objects.all(),
+    #     view_name="collection-detail",
+    # )
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
