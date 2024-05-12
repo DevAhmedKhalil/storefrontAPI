86cafabe544077c27ff5cd97730bdef48d0cd6d5
DevAhmedKhalil <akhalil01@gmail.com>
2024-05-13 01:54:55 +0300

    Finish Collections API.

--- store/serializers.py
+++ store/serializers.py
@@ -6,10 +6,9 @@ from rest_framework import serializers
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
-        fields = ["id", "title"]
+        fields = ["id", "title", "products_count"]
 
-    # id = serializers.IntegerField()
-    # title = serializers.CharField(max_length=255)
+    products_count = serializers.IntegerField()
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -25,25 +24,8 @@ class ProductSerializer(serializers.ModelSerializer):
             "price_with_tax",
             "collection",
         ]
-        # fields = "__all__" # this is for lazy developers
 
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
-
-    # def validate(self, data):
-    #     if data["password"] != data["confirm_password"]:
-    #         return serializers.ValidationError("Password Don't Match.")
-    #     return data
-
-    # def create(self, validated_data):
-    #     product = Product(**validated_data)
-    #     product.other = 1  # special field
-    #     product.save()
-    #     return product
-
-    # def update(self, instance, validated_data):
-    #     instance.unit_price = validated_data.get("unit_price")
-    #     instance.save()
-    #     return instance
