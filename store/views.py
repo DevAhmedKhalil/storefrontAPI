136242fcb539152a208bb547ffa465f16da6c82b
DevAhmedKhalil <akhalil01@gmail.com>
2024-05-14 19:00:21 +0300

    sec-2 class-based views

--- store/views.py
+++ store/views.py
@@ -3,42 +3,62 @@ from django.http import HttpResponse
 from django.db.models import Count
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import status
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
 
-# Create your views here. View Function:
-@api_view(["GET", "POST"])
-def product_list(request):
-    if request.method == "GET":
+# Class_Based View
+class ProductList(APIView):
+    def get(self, request):
         queryset = Product.objects.select_related("collection").all()
         serializer = ProductSerializer(
             queryset, many=True, context={"request": request}
         )
         return Response(serializer.data)
-    elif request.method == "POST":
+
+    def post(self, request):
         serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(
-            raise_exception=True
-        )  # raise exception replaced if(is_valid) else block
+        serializer.is_valid(raise_exception=True)
         serializer.save()
-        # print(serializer.validated_data) # we don't need cuz save method has same logic
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(["GET", "PUT", "DELETE"])
-def product_detail(request, id):
-    product = get_object_or_404(Product, pk=id)
-    if request.method == "GET":
+# @api_view(["GET", "POST"])
+# def product_list(request):
+#     if request.method == "GET":
+#         queryset = Product.objects.select_related("collection").all()
+#         serializer = ProductSerializer(
+#             queryset, many=True, context={"request": request}
+#         )
+#         return Response(serializer.data)
+#     elif request.method == "POST":
+#         serializer = ProductSerializer(data=request.data)
+#         serializer.is_valid(
+#             raise_exception=True
+#         )  # raise exception replaced if(is_valid) else block
+#         serializer.save()
+#         # print(serializer.validated_data) # we don't need cuz save method has same logic
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ProductDetail(APIView):
+
+    def get(self, request, id):
+        product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-    elif request.method == "PUT":
+
+    def put(self, request, id):
+        product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    elif request.method == "DELETE":
+
+    def delete(self, request, id):
+        product = get_object_or_404(Product, pk=id)
         if product.orderitems.count() > 0:
             return Response(
                 {"Error": "This product has order items."},
@@ -46,6 +66,27 @@ def product_detail(request, id):
             )
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# @api_view(["GET", "PUT", "DELETE"])
+# def product_detail(request, id):
+#     product = get_object_or_404(Product, pk=id)
+#     if request.method == "GET":
+#         serializer = ProductSerializer(product)
+#         return Response(serializer.data)
+#     elif request.method == "PUT":
+#         serializer = ProductSerializer(product, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+#     elif request.method == "DELETE":
+#         if product.orderitems.count() > 0:
+#             return Response(
+#                 {"Error": "This product has order items."},
+#                 status=status.HTTP_400_BAD_REQUEST,
+#             )
+#         product.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(["GET", "POST"])
