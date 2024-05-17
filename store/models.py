cfb332d8804622f8c0629b48d2ef845b935a87b1
DevAhmedKhalil <akhalil01@gmail.com>
2024-05-17 21:08:06 +0300

    Solving store_review Table in DB.

--- store/models.py
+++ store/models.py
@@ -111,7 +111,7 @@ class CartItem(models.Model):
 
 class Review(models.Model):
     product = models.ForeignKey(
-        Product, on_delete=models.CASCADE, related_name="reviews"
+        "Product", on_delete=models.CASCADE, related_name="reviews"
     )
     name = models.CharField(max_length=255)
     description = models.TextField()
