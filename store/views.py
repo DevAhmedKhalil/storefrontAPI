00dd710df6f833026049402fbdff78dc4e74ebc6
DevAhmedKhalil <akhalil01@gmail.com>
2024-05-17 23:47:05 +0300

    searching and ordering in products.

--- store/views.py
+++ store/views.py
@@ -1,12 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.decorators import api_view
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 from store.filters import ProductFilter
@@ -17,9 +12,10 @@ from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializ
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
-    # filterset_fields = ["collection_id"]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    search_fields = ["title", "description"]
+    ordering_fields = ["unit_price", "last_update"]
 
     def get_serializer_context(self):
         return {"request": self.request}
