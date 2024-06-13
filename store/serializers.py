5ae869d5ead6c218fd54f954bb44b12de9266557
DevAhmedKhalil <akhalil01@gmail.com>
2024-06-14 02:23:01 +0300

    FINISH Section 2.

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.db import transaction
 from rest_framework import serializers
+from .signals import order_created
 from .models import (
     CartItem,
     Customer,
@@ -184,5 +185,9 @@ class CreateOrderSerializer(serializers.Serializer):
                 for item in cart_items
             ]
             OrderItem.objects.bulk_create(order_items)
+
             Cart.objects.filter(pk=cart_id).delete()
+
+            order_created.send_robust(self.__class__, order=order)
+
             return order
